6845eb7de352d5f7324e7c775551c3b7aa10dde1
ian-iglesias <ianiglesias6798@gmail.com>
2023-02-14 09:07:42 -0300

    resolucao 1 e 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isTerminouEnsinoMedio;
-let isCursandoOutraFaculdade;
+let idade = 18;
+let isTerminouEnsinoMedio = true;
+let isCursandoOutraFaculdade = true; 
 
-if(){
-
+if(idade >= 18){
+    alert("A pessoa já é maior de idade!")
 }else{
-	
-}
+    alert("A pessoa não é maior de idade!")
+};
+
+if(isTerminouEnsinoMedio = true){
+    alert("A pessoa já terminou o ensino médio!")
+}else{
+    alert("A pessoa não terminou o ensino médio!")
+};
+
+if(isCursandoOutraFaculdade = false){
+    alert("A pessoa não está cursando nenhuma faculdade!")
+}else{
+    alert("A pessoa está cursando uma faculdade!")
+};
